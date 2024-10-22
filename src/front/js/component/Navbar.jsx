ffff2cdb00d1647e86c1923d71d523f1dbdb800e
Jose Alejandro Moreno <166447988+JochiKum@users.noreply.github.com>
2024-10-22 01:25:52 +0000

    Process: Navbar

--- src/front/js/component/Navbar.jsx
+++ src/front/js/component/Navbar.jsx
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 
 export const Navbar = () => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+
+	const toggleDropdown = () => {
+		setDropdownOpen(!dropdownOpen);
+	};
+
 	return (
-		<nav className="navbar navbar-light bg-light">
-			<div className="container">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
-			</div>
+		<nav className="navbar">
+			<div className="logo">PowerPulse</div>
+			<ul className="nav-links m-0">
+				<li><a href="#home">Inicio</a></li>
+				<li><a href="#about">Sobre Nosotros</a></li>
+				<li>
+					<div className="dropdown">
+						<button onClick={toggleDropdown} className="dropbtn">Servicios</button>
+						{dropdownOpen && (
+							<div className="dropdown-content ">
+								<a href="#personal-training">Entrenamiento Personal</a>
+								<a href="#nutrition">Nutrición</a>
+								<a href="#group-classes">Clases Grupales</a>
+							</div>
+						)}
+					</div>
+				</li>
+				<li><a href="#contact">Contacto</a></li>
+			</ul>
+			<button className="cta-button">Únete Ahora</button>
 		</nav>
 	);
-};
+}
