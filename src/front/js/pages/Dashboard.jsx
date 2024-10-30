e8babe80d1224a1048cebcb182055a317e9b3d68
Mariana Hueck <marianahueck@gmail.com>
2024-10-30 02:21:00 +0000

    wip

--- src/front/js/pages/Dashboard.jsx
+++ src/front/js/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import { CardInfo } from '../component/CardInfo.jsx'
 import { CardIndicator } from '../component/CardIndicator.jsx'
 import "../../styles/dashboard.css"
 import { Link } from 'react-router-dom'
+import { CreateCard } from '../component/CreateCard.jsx'
 
 
 export const Dashboard = () => {
@@ -16,25 +17,36 @@ export const Dashboard = () => {
         <div className='overview-cardIndicator'>
           <CardIndicator value={store?.trainingPlans?.results?.length ?? "0"} description={"Planes de Ejercicio"} section={"planCount"} />
         </div>
-        {/* <div className='overview-cardIndicator'>
-          <CardIndicator value={"mariana"} description={"Cantidad de Miembros"} section={"membersCount"} />
+        <div className='overview-cardIndicator'>
+          <CardIndicator value={"0"} description={"Cantidad de Miembros"} section={"membersCount"} />
         </div>
         <div className='overview-cardIndicator'>
-          <CardIndicator value={"mariana"} description={"Tiempo aproximado por ejercicio"} section={"timeCount"} />
+          <CardIndicator value={"0"} description={"Tiempo aproximado por ejercicio"} section={"timeCount"} />
         </div>
         <div className='overview-cardIndicator'>
-          <CardIndicator value={"mariana"} description={"Metas Alcanzadas"} section={"percentageGoal"} />
-        </div> */}
+          <CardIndicator value={"0"} description={"Metas Alcanzadas"} section={"percentageGoal"} />
+        </div>
       </div>
       <div className='main-cards'>
 
-        {store?.trainingPlans?.results?.length ? (
+        {/* {store?.trainingPlans?.results?.length ? ( */}
 
-          <div className='cardx'><CardInfo title={"Planes de Ejercicio"} subtitle={"Los Mejores planes de Ejercicios"} description={"Aqui puedes encontrar un plan que se adapte a tus necesidades"} /></div>
-        ) : (<div style={{ color: "white" }}>Crea tu Primer Plan de Ejercicio <Link to={"/create-plan"}>aqui</Link></div>)}
-        {/* <div className='cardx'><CardInfo title={"titulo modelo2"} subtitle={"algun mensaje motivador2"} description={"algun mensaje"} /></div> */}
-        {/* <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador3"} description={"algun mensaje"} /></div> */}
-        {/* <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador4"} description={"algun mensaje"} /></div> */}
+        <div>
+          <CardInfo title={"Planes de Ejercicio"} subtitle={"Los Mejores planes de Ejercicios"} description={"Aqui puedes encontrar un plan que se adapte a tus necesidades"} />
+        </div>
+        {/* // ) : (<CreateCard />)} */}
+        <div>
+          <CreateCard />
+          {/* <CardInfo title={"titulo modelo2"} subtitle={"algun mensaje motivador2"} description={"algun mensaje"} /> */}
+        </div>
+        <div>
+          {/* <CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador3"} description={"algun mensaje"} /> */}
+          <CreateCard />
+        </div>
+        <div>
+          {/* <CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador4"} description={"algun mensaje"} /> */}
+          <CreateCard />
+        </div>
       </div>
     </main>
   )
