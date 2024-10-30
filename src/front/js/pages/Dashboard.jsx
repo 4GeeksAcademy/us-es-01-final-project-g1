d8eaac3b4b77815b46b39bd5f518456e2bab6769
mamel210 <mamel210@gmail.com>
2024-10-30 01:03:23 +0000

    [createPlan] wip

--- src/front/js/pages/Dashboard.jsx
+++ src/front/js/pages/Dashboard.jsx
@@ -1,47 +1,41 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Context } from '../store/appContext.js'
 import { CardInfo } from '../component/CardInfo.jsx'
-import "../../styles/dashboard.css"
 import { CardIndicator } from '../component/CardIndicator.jsx'
+import "../../styles/dashboard.css"
+import { Link } from 'react-router-dom'
+
 
 export const Dashboard = () => {
+  const { store, } = useContext(Context)
+
+
   return (
-    <div className='grid-conxtainer'>
-
-
-
-
-
-
-      <main className='maixn'>
-        {/* <div className='main-header'>
-          <div className='main-header__heading'>Hello User, stats</div>
-          <div className='main-header__updates'>Badge indicadores, quizas hardcoding</div>
+    <main>
+      <div className='main-overview'>
+        <div className='overview-cardIndicator'>
+          <CardIndicator value={store?.trainingPlans?.results?.length ?? "0"} description={"Planes de Ejercicio"} section={"planCount"} />
+        </div>
+        {/* <div className='overview-cardIndicator'>
+          <CardIndicator value={"mariana"} description={"Cantidad de Miembros"} section={"membersCount"} />
+        </div>
+        <div className='overview-cardIndicator'>
+          <CardIndicator value={"mariana"} description={"Tiempo aproximado por ejercicio"} section={"timeCount"} />
+        </div>
+        <div className='overview-cardIndicator'>
+          <CardIndicator value={"mariana"} description={"Metas Alcanzadas"} section={"percentageGoal"} />
         </div> */}
+      </div>
+      <div className='main-cards'>
 
-        <div className='main-overview'>
-          <div className='overview-cardIndicator'>
-            <CardIndicator value={"mariana"} description={"hueck"} section={"planCount"} />
-          </div>
-          <div className='overview-cardIndicator'>
-            <CardIndicator value={"mariana"} description={"hueck"} section={"membersCount"} />
-          </div>
-          <div className='overview-cardIndicator'>
-            <CardIndicator value={"mariana"} description={"hueck"} section={"timeCount"} />
-          </div>
-          <div className='overview-cardIndicator'>
-            <CardIndicator value={"mariana"} description={"hueck"} section={"percentageGoal"} />
-          </div>
-        </div>
+        {store?.trainingPlans?.results?.length ? (
 
-        <div className='main-cards'>
-          <div className='cardx'><CardInfo title={"titulo modelo1"} subtitle={"algun mensaje motivador1"} description={"algun mensaje"} /></div>
-          <div className='cardx'><CardInfo title={"titulo modelo2"} subtitle={"algun mensaje motivador2"} description={"algun mensaje"} /></div>
-          <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador3"} description={"algun mensaje"} /></div>
-          <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador4"} description={"algun mensaje"} /></div>
-        </div>
-      </main>
-
-
-    </div>
+          <div className='cardx'><CardInfo title={"Planes de Ejercicio"} subtitle={"Los Mejores planes de Ejercicios"} description={"Aqui puedes encontrar un plan que se adapte a tus necesidades"} /></div>
+        ) : (<div style={{ color: "white" }}>Crea tu Primer Plan de Ejercicio <Link to={"/create-plan"}>aqui</Link></div>)}
+        {/* <div className='cardx'><CardInfo title={"titulo modelo2"} subtitle={"algun mensaje motivador2"} description={"algun mensaje"} /></div> */}
+        {/* <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador3"} description={"algun mensaje"} /></div> */}
+        {/* <div className='cardx'><CardInfo title={"titulo modelo3"} subtitle={"algun mensaje motivador4"} description={"algun mensaje"} /></div> */}
+      </div>
+    </main>
   )
 }
